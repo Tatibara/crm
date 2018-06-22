76462d6016aaf01212f12ce6170c248acfc4c01e
Tatiana Baradari <ttorgonskaya@gmail.com>
2018-06-22 16:24:48 +0200

    added React Router

--- src/app.js
+++ src/app.js
@@ -1,9 +1,19 @@
-import 'normalize.css/normalize.css';
-
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
+import 'normalize.css/normalize.css';
 import CRMApp from './components/CRMApp';
-
 import './styles/styles.scss';
 
-ReactDOM.render(<CRMApp />, document.getElementById('app'));
+const Home = () => <h1>Home Page</h1>;
+
+const routes = (
+  <BrowserRouter>
+    <div>
+      <Route path="/" component={Home} exact />
+      <Route path="/crm" component={CRMApp} />
+    </div>
+  </BrowserRouter>
+);
+
+ReactDOM.render(routes, document.getElementById('app'));
--- webpack.config.js
+++ webpack.config.js
@@ -28,6 +28,7 @@ module.exports = {
   devtool: 'cheap-module-eval-source-map',
 
   devServer: {
-    contentBase: path.resolve(__dirname, 'public')
+    contentBase: path.resolve(__dirname, 'public'),
+    historyApiFallback: true
   }
 };
