c2651ac351731265c76d9e2dd1e1c552a0096ed8
Tatiana Baradari <ttorgonskaya@gmail.com>
2018-06-25 10:26:09 +0200

    added NotFoundPage

--- src/app.js
+++ src/app.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import CRMApp from './components/CRMApp';
+import NotFoundPage from './components/NotFoundPage';
 import './styles/styles.scss';
 
 const Home = () => <h1>Home Page</h1>;
 
 const routes = (
   <BrowserRouter>
-    <div>
+    <Switch>
       <Route path="/" component={Home} exact />
       <Route path="/crm" component={CRMApp} />
-    </div>
+      <Route component={NotFoundPage} />
+    </Switch>
   </BrowserRouter>
 );
 
